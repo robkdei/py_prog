193ac842cb2c6f1cffe3b5d885f5e02605d9b2fc
Rob Hayes <rob@kdei.co.uk>
2022-09-06 10:04:40 +0100

    Commit of first programming tests

--- project_one.py
+++ project_one.py
@@ -10,9 +10,21 @@ def swap_if_out_of_order(a, b):
     return None
 
 
+def list_length(items):
+    return None
+
+
 def sublist(items, start, end):
     return None
 
 
 def is_present(items, item):
+    return None
+
+
+def is_not_present(items, item):
+    return None
+
+
+def return_greeting(name):
     return None
--- test_project_one.py
+++ test_project_one.py
@@ -14,6 +14,10 @@ def test_swap_if_out_of_order():
     assert project_one.swap_if_out_of_order(3, 4) == [3, 4]
 
 
+def test_list_length():
+    assert project_one.list_length(["oranges", "apples", "pears", "grapes"]) == 4
+
+
 def test_sublist():
     assert project_one.sublist(["oranges", "apples", "pears", "grapes"], 2, 4) == ["pears", "grapes"]
 
@@ -21,3 +25,12 @@ def test_sublist():
 def test_is_present():
     assert not project_one.is_present(["oranges", "apples", "pears", "grapes"], "strawberries")
     assert project_one.is_present(["oranges", "apples", "pears", "grapes"], "oranges")
+
+
+def test_is_no_present():
+    assert project_one.is_present(["oranges", "apples", "pears", "grapes"], "strawberries")
+    assert not project_one.is_present(["oranges", "apples", "pears", "grapes"], "oranges")
+
+
+def test_return_greeting():
+    assert project_one.return_greeting("Rob") == "Hello, Rob"
