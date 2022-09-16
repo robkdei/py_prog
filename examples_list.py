88340ff37524efa39b1c11c8934413e29d3a5569
Henry Hayes <hempyhay@outlook.com>
2022-09-16 17:07:27 +0100

    Lists Examples

--- examples_list.py
+++ examples_list.py
@@ -1,14 +1,14 @@
 def list_length(items):
-    return None
+    return len(items)
 
 
 def slice(items, start, end):
     return items[start:end]
 
 
-def is_present(items, item):
-    return None
+def is_present(items, item_to_find):
+    return item_to_find in items
 
 
-def is_not_present(items, item):
-    return None
+def is_not_present(items, item_to_find):
+    return item_to_find not in items
